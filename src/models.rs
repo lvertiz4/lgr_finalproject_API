14b77ba9ff77ed3654825ae0843b4e186693ed06
lvertiz4 <lvertiz@gmail.com>
2023-06-30 18:32:31 -0400

    finished Stage 1 Step 1

---
+++ src/models.rs
@@ -0,0 +1,66 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Question {
+    //TODO: add a public 'title' field of type String to
+    //TODO: add a public 'description' field of type String
+    pub title: String,
+    pub description: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+pub struct QuestionDetail {
+    //TODO: add a public 'question_uuid" field of type String
+    //TODO: add a public 'title' field of type String
+    //TODO: add a public 'description' field of type String
+    //TODO: add a public 'created_at' field of type String
+    pub question_uuid: String,
+    pub title: String,
+    pub description: String,
+    pub created_at: String,
+}
+
+//TODO: create a struct called 'QuestionID'
+// derive the following traits: Serialized, Deserialized
+//add a public 'question_uuid' field of type String
+//add a public 'content' field of type String
+
+#[derive(Serialize, Deserialize)]
+pub struct QuestionID {
+    pub question_uuid: String,
+    pub content: String,
+}
+
+//TODO: create a struct called 'Answer'
+// derive the following traits: Serialized, Deserialized
+// add a public 'question_uuid' field of type String
+// add a public 'content' field of type String
+
+#[derive(Serialize, Deserialize)]
+pub struct Answer {
+    pub question_uuid: String,
+    pub content: String,
+}
+
+//TODO: create a struct called 'AnswerDetail'
+//derive the following traits: Serialized, Deserialize, Debug, Clone, PartialEq
+// add a public 'answer_uuid' field of type String
+// add a public 'question_uuid' field of type String
+// add a public 'content' field of type String
+// add a public 'create_at' field of type String
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct AnswerDetail {
+    pub answer_uuid: String,
+    pub question_uuid: String,
+    pub content: String,
+    pub created_at: String,
+}
+
+//TODO: create a struct called 'AnswerID'
+// derive the following traits: Serialized, Deserialize
+//add a public 'answer_id' field of type String
+#[derive(Serialize, Deserialize)]
+pub struct AnswerID {
+    pub answer_uuid: String,
+}
